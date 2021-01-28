6c3dcf5052cda16c76cd8d76027a658965787919
ankushduacodes <ankushduacodes@gmail.com>
2021-01-28 14:53:26 +0530

    add requirements file to deploy

--- scraper_module/email/email_handler.py
+++ scraper_module/email/email_handler.py
@@ -10,8 +10,8 @@ def send_email(message):
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
+        server.ehlo_or_helo_if_needed()
         server.starttls(context=context)
-        server.ehlo()  # Can be omitted
+        server.ehlo_or_helo_if_needed()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
